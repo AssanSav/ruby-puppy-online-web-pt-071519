838c3b2c0e6fe23cd6ed0388fdec264ee7da3ee4
assane savadogo <assane.savadogo81@gmail.com>
2019-08-28 14:06:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,30 +1,4 @@
 require 'pry'
 class Dog 
-  attr_accessor :name
-  attr_reader :save
-  @@all = []
-  
-  def initialize(name)
-    @name = name 
-    #@@all << self
-    self.save
-  end
-  
-  def self.all 
-    @@all 
-  end
-  
-  def self.print_all 
-    @@all.each do |dog|
-      puts dog.name 
-    end
-  end
-  
-  def save 
-    @@all << self 
-  end
-  
-  def self.clear_all 
-    @@all.clear 
-  end 
+ 
 end
