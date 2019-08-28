f0b2453fef7a714c362d94dea18f7d186d978e89
assane savadogo <assane.savadogo81@gmail.com>
2019-08-28 14:11:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,21 @@
 require 'pry'
 class Dog 
+  
+  @@all = []
  
+ def initialize(name)
+   @name = name 
+ end
+ 
+ def self.all 
+   @@all 
+ end
+ 
+ def self.clear 
+   @@all.clear 
+ end
+ 
+ def self.print 
+   
+ end
 end
